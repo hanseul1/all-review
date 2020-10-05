a3bd305f053ce480ffd34388f90de6aca1d4f562
hanseul1 <hsdeb11@gmail.com>
2020-10-05 14:16:04 +0900

    Add setting files for connecting Travis-CI & CodeDeploy & EC2

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=all-review
+
+echo "> Copy Build File"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> Check Running Application PID"
+
+CURRENT_PID=$(pgrep -fl all-review | grep jar | awk '{print $1}')
+
+echo "Running Application PID : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+	echo "> There's no running application."
+else
+	echo "> Kill $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> New Application Deployment"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> Jar Name : $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+echo "> Execute $JAR_NAME"
+
+nohup java -jar \-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-prod-db.properties \-Dspring.profiles.active=production \ $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
